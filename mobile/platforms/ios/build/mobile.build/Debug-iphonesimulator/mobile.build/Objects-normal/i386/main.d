be75170c543f1420aa7177389b772ca821b19c25
Oscar4 <oxkar4@gmail.com>
2016-06-03 10:28:18 -0600

    iOS add

---
+++ mobile/platforms/ios/build/mobile.build/Debug-iphonesimulator/mobile.build/Objects-normal/i386/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/omnimedia/Desktop/my-instagram/mobile/platforms/ios/internal/main.m \
+  /Users/omnimedia/Desktop/my-instagram/mobile/platforms/ios/internal/NativeScript/include/NativeScript.h \
+  /Users/omnimedia/Desktop/my-instagram/mobile/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
+  /Users/omnimedia/Desktop/my-instagram/mobile/platforms/ios/internal/NativeScript/include/TNSRuntime+Diagnostics.h \
+  /Users/omnimedia/Desktop/my-instagram/mobile/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
+  /Users/omnimedia/Desktop/my-instagram/mobile/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/omnimedia/Desktop/my-instagram/mobile/platforms/ios/internal/TNSDebugging.h
